80106e133cc5657d52507f2aa4cd1ac0776062fe
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-09-09 20:08:40 +0700

    [CRUD] delete product

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -32,5 +32,10 @@ class ProductController {
       .then(() => res.redirect("/me/stored/products"))
       .catch(next);
   }
+  delete(req, res, next) {
+    Product.deleteOne({ _id: req.params.id })
+      .then(() => res.redirect("back"))
+      .catch(next);
+  }
 }
 module.exports = new ProductController();
